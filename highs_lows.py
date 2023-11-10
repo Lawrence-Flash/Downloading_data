2cdbd021be0217bf05a2e1d91163e87392a427c8
Lawrence-Flash <lawrencetshabalala253@gmail.com>
2023-11-10 11:00:27 +0200

    new file added

--- highs_lows.py
+++ highs_lows.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 
 def main():
-    filename = 'sitka_weather_07-2014.csv'
+    filename = 'sitka_weather_2014.csv'
     #header_positions(filename)
     Extract_Read_Data(filename)
     
@@ -19,31 +19,34 @@ def header_positions(file):
     
 
 def Extract_Read_Data(file):
-    # Get dates and high temperature from file
+    # Get dates, high  and low temperature from file
     with open(file) as f:
         reader = csv.reader(f)
         header_row = next(reader)
         
-        dates, highs = [], []
+        dates, highs, lows = [], [], []
         for row in reader:
             current_date = datetime.strptime(row[0], "%Y-%m-%d")
             dates.append(current_date)
             
             high = int(row[1])
             highs.append(high)
+            
+            low = int(row[3])
+            lows.append(low)
         
         # call the function that plots the data in a line a graph plot
-        plot_Data(highs,dates)
+        plot_Data(lows,highs,dates)
     
 # this function is responsible for ploting the Data
-def plot_Data(temprature, DATE):
+def plot_Data(low_temperature,high_temprature, DATE):
     
     # Plot the data.
     fig = plt.figure(dpi=128, figsize=(10, 6))
-    plt.plot(DATE, temprature, c='red')
-    
+    plt.plot(DATE, high_temprature, c='red')
+    plt.plot(DATE, low_temperature, c='blue')
     # Format plot
-    plt.title("Daily high temperatures, july 2014", fontsize=24)
+    plt.title("Daily high and low temperatures - 2023", fontsize=24)
     plt.xlabel('', fontsize=16)
     # Draw the date labels diagonally to prevent them from overlapping
     fig.autofmt_xdate()
